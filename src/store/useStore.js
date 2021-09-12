5614bca4208f1fcdc0c377b647ff41e6171fa066
eman-e-alatawi <eman.eid.alatawi@gmail.com>
2021-09-12 17:53:07 +0300

    use devtools from zustand middleware

--- src/store/useStore.js
+++ src/store/useStore.js
@@ -1,13 +1,15 @@
 import create from 'zustand'
+import {devtools} from 'zustand/middleware'
 
 import { egyptianMealsSlice } from './egyptianMealsSlice'
 import { italianMealsSlice } from './italianMealsSlice'
 import { indianMealsSlice } from './indianMealsSlice'
 
-const useStore = create((set) => ({
+const store = (set) => ({
     ...egyptianMealsSlice(set),
     ...italianMealsSlice(set),
     ...indianMealsSlice(set)
-}))
+})
+const useStore = create(devtools(store))
 
 export default useStore
